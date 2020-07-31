cd4dd5250da34254234ec126d799b6bed6617bcf
AakashMallik <aakashs.mallik.cse15@itbhu.ac.in>
2020-07-31 12:29:00 +0530

    [AakashMallik] Default config in exp4

--- train.sh
+++ train.sh
@@ -34,11 +34,11 @@ GPU_IDX=0
 # CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="exp_3_g.yml" -p
 
 # Dummy run
-CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="debug_train_1.yml" --dev -p
+# CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="debug_train_1.yml" --dev -p
 # CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="debug_train_2.yml" --dev -p
 # CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="exp_1.yml" --dev -p
 # CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="exp_2.yml" --dev -p
 # CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="exp_3.yml" --dev -p
 
 # 2080Ti Basic run
-# CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="exp_4_a.yml" --dev
+CUDA_VISIBLE_DEVICES=$GPU_IDX python3 main.py --config="exp_4_a.yml" --dev -p
